30c79275a90b1e5a4b0d51a7de2ae70d509e304e
Chris Anderson <gca2@earthlink.net>
2019-12-16 23:22:04 -0800

    Experiments with Realsense

--- rvr.py
+++ rvr.py
@@ -18,6 +18,14 @@ from sphero_sdk import SerialAsyncDal
 from sphero_sdk import SpheroRvrAsync
 from sphero_sdk.common.enums.drive_enums import DriveFlagsBitmask
 
+ROIx1 = 200
+ROIx2 = 400
+ROIy1 = 0
+ROIy2 = 10
+depth1 = 0
+depth2 = 1
+
+
 loop = asyncio.get_event_loop()
 rvr = SpheroRvrAsync(
     dal=SerialAsyncDal(
@@ -38,35 +46,34 @@ async def main():
     await rvr.reset_yaw()
     print("rvr ready!")
     # issue the driving command
+    scan = []
+    for x in range(ROIx1,ROIx2):
+        scan.append(False)
     while True:
         frames = pipeline.wait_for_frames()
         depth = frames.get_depth_frame()
-        if depth:
-            dist = depth.get_distance(100, 100)
-            print(dist)
-        flags = 0
-        if reverse:
-            flags = DriveFlagsBitmask.drive_reverse
-        else:
-            flags=DriveFlagsBitmask.none.value
-        await rvr.drive_with_heading(speed, heading, flags)
-        # sleep the infinite loop for a 10th of a second to avoid flooding the serial port.
-        await asyncio.sleep(0.1)
-
-
-# Implement two "processing" functions, each of which
-# occassionally lags and takes longer to process a frame.
-def slow_processing(frame):
-    n = frame.get_frame_number()
-    if n % 20 == 0:
-        time.sleep(1/4)
-    print(n)
-
-def slower_processing(frame):
-    n = frame.get_frame_number()
-    if n % 20 == 0:
-        time.sleep(1)
-    print(n)
+        if not depth: continue  # just do the loop again until depth returns true
+        for y in range(ROIy1,ROIy2):
+            for x in range(ROIx1, ROIx2):
+                dist = depth.get_distance(x, y)
+                if depth1 < dist and dist < depth2:
+                    scan[x-ROIx1] = True
+                else:
+                    scan[x-ROIx1] = False
+            for x in range(ROIx1, ROIx2):
+                if scan[x-ROIx1]:
+                    print("x", end = '')
+                else:
+                    print(" ", end = '')
+            print("\n")
+        # flags = 0
+        # if reverse:
+        #     flags = DriveFlagsBitmask.drive_reverse
+        # else:
+        #     flags=DriveFlagsBitmask.none.value
+        # await rvr.drive_with_heading(speed, heading, flags)
+        # # sleep the infinite loop for a 10th of a second to avoid flooding the serial port.
+        # await asyncio.sleep(0.1)
 
 
 try:
@@ -85,6 +92,6 @@ try:
 except KeyboardInterrupt:
         print("Keyboard Interrupt...")
         key_helper.end_get_key_continuous()
-finally:
-        print("Press any key to exit.")
-        exit(1)
+# finally:
+#         print("Press any key to exit.")
+#         exit(1)
